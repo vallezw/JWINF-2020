86f7ead021c111521b68989ba7418b22531ecd5c
Valentin Zwerschke <v.zwerschke@mail.de>
2020-10-03 17:52:10 +0200

    add algorithmus word file

--- Passwoerter/Passwd.py
+++ Passwoerter/Passwd.py
@@ -1,16 +1,18 @@
 import random
 from random import randrange
 from random import choice
-#import string
+
+# import string
 
 # Regeln, um ein ausprechbares Wort zu erstellen:
-# 1. Maximal 2 Zahlen hintereinander
-# 2. Gleiche Buchstaben müssen mindesterns 3 Zeichen Abstand haben
-# 3. Anfangsbuchstabe muss groß geschrieben sein
-# Vokal dann konsonant im abwechseln
+# 1. Geringe Wahrscheinlichkeit, dass 2 Zahlen hintereinander kommen, noch geringer mehr als 2
+# 2. Keine zwei gleichen Buchstaben hintereinander
+# 3. Anfang immer mit Buchstabe, mit 75% Wahrscheinlichkeit groß, sonst klein
+# 4. Nach Vokal immer Konsonant oder Sonderzeichen/Zahl, nach Konsonant mit hoher Wahrscheinlichkeit Vokal
+# 5. mit geringer Wahrscheinlichkeit nach Vokal ein e, liefert ae oe ue ee ie, alles existierende Kombis
 
 # Vokal Vok Kons Vok kons vok kons vok kons
-# Sch sp st hinzufügen
+
 
 konsonanten = ["B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"]
 vokale = ["A", "E", "I", "O", "U"]
@@ -22,20 +24,22 @@ def get_new_random_string(length):
 
     before_letter = None
     kons = False
-
     has_sonder = False
 
     how_many_sonder = 0
 
-    for i in range(0, length):
+    #  TODO: Mindetslänge checken
+    #  TODO: Am Ende prüfen, ob eine Zahl drin. Wenn nicht, letztes Zeichen Zahl (als Option, wenn Passwortregel das fordert)
+    #  TODO: Nach Zahl mit 50% Wahrscheinlichkeit Groß oder Klein, 75% am PasswortAnfang
 
+    for i in range(0, length):
         if has_sonder:
             if how_many_sonder > 0:
                 how_many_sonder -= 1
                 continue
 
-        if before_letter is None:
-            if bool(random.getrandbits(1)):
+        if before_letter is None: # Fangen wir mit dem ersten Zeichen an ... entweder Vokal oder Konsonant, entweder groß oder klein
+            if bool(random.getrandbits(1)):  # True oder False
                 if bool(random.getrandbits(1)):
                     before_letter = choice(vokale)
                 else:
@@ -48,18 +52,19 @@ def get_new_random_string(length):
                     before_letter = choice(konsonanten).lower()
                 kons = True
 
-            password += before_letter
+            password += before_letter # hiermit festlegen, dass erste Zeichen fertig ist
 
-        else:
-            if random.randint(0, 10) == 10:
+        else: # jetzt sind wir beim zweiten oder allen Folgezeichen
+            if random.randint(1, 10) == 10: # Mit Wahrscheinlichkeit 1/10 wird eine Zahl genommen
                 letter = str(randrange(0, 9))
+                kons = bool(random.getrandbits(1))
 
-            elif kons:
+            elif kons: # Wenn vorheriger Buchstabe Konsonant war, wird der nächste ein Vokal
                 letter = get_vokal_letter(before_letter)
                 kons = False
 
             else:
-                if random.randint(0, 1) == 1 and not has_sonder and length > 7 and i < length - 3:
+                if bool(random.getrandbits(1)) and not has_sonder and length > 7 and i < length - 3:
                     letter = choice(SonderLetter)
                     if letter == "SCH":
                         how_many_sonder = 2
@@ -69,13 +74,12 @@ def get_new_random_string(length):
                     kons = True
                     has_sonder = True
 
-                elif random.randint(0, 10) == 10:
-                    letter = str(randrange(0, 9))
+            #   elif random.randint(0, 10) == 10:
+            #       letter = str(randrange(0, 9))
 
-                elif random.randint(0, 8) == 8:
+                elif random.randint(1, 8) == 8:
                     letter = get_vokal_letter(before_letter)
                     kons = False
-
                 else:
                     letter = get_kons_letter(before_letter)
                     kons = True
@@ -99,4 +103,5 @@ def get_vokal_letter(before_letter):
         letter = choice(vokale)
     return letter
 
-print(get_new_random_string(18))
+
+print(get_new_random_string(10))
