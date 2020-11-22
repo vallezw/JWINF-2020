6489be5182de163e628c8d3611287d0fb116b55f
Valentin Zwerschke <v.zwerschke@mail.de>
2020-11-22 13:26:13 +0100

    edit

--- Woerter Zaehlen/main.py
+++ Woerter Zaehlen/main.py
@@ -1,8 +1,8 @@
+import re
+
 file = open("test1.txt")
 missing_words_string = file.readline()
 words_list = sorted(file.readline().split(), key=len)
-
-print(words_list)
 
 dict = {} # Key sind die Anzahl der buchstaben und value eine Liste von den Strings die so viele buchstaben haben
 length = 0
@@ -13,8 +13,6 @@ for x in words_list:
 
     dict[length].append(x)
 
-print(dict)
-
 miss_split = missing_words_string.replace(',', '').replace('!', '').replace('.', '').replace('?', '.').split()
 
 miss_dict = {}
@@ -24,19 +22,35 @@ for x in miss_split:
         miss_dict.update({length: []})
 
     miss_dict[length].append(x)
-print(miss_dict)
 
+final_dict = {}
 for key, value in miss_dict.items():
     for miss_wort in value:
-        letter = miss_wort.replace('_', '')#
+        letter = miss_wort.replace('_', '')
         used_index = []
+#        last_word = ''
+        used_words = []
         if letter == '':
             last_word = miss_wort
             continue
         index = miss_wort.find(letter)
         for x in dict[key]:
             if x[index] == letter:
-                print('trueee')
                 used_index.append(index)
-                print(x, miss_wort)
-        if last_word
+                final_dict[miss_wort] = x
+                used_words.append(x)
+
+    find_last_word = [x for x in dict[key] if x not in used_words]
+    if len(find_last_word) > 0:
+        final_dict[last_word] = find_last_word[0]
+        #print([x for x in dict[key] if x not in used_words][0], last_word)
+
+
+
+def replace_all(text, dic):
+    for i, j in dic.items():
+        text = re.sub(r'\b'+i+r'\b', j, text)
+
+    return text
+
+print(replace_all(missing_words_string, final_dict))
